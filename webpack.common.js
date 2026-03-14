4d791abcfce1c4aca3964da242862a31e2a74e66
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-14 14:26:07 +0100

    Init template

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  scripts: {
+    build: "webpack --config webpack.prod.js",
+    dev: "webpack serve --config webpack.dev.js",
+    deploy: "git subtree push --prefix dist origin gh-pages",
+  },
+};
